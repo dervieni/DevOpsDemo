5229f42f0f3ca3f92c632adc525f5ec87f181322
ed <>
2023-03-27 12:55:45 +0200

    Unit Tests: Erster Code für die Even Operation

--- backend/src/main/java/ch/zhaw/iwi/devops/MathLab/MathLab.java
+++ backend/src/main/java/ch/zhaw/iwi/devops/MathLab/MathLab.java
@@ -6,4 +6,13 @@ public class MathLab {
         int sum = a + b;
         return (sum);
     }
+
+    public String even(int a, int b) {
+        Boolean Even=sum(a, b)%2==0;
+        if (Even==true) {
+            return("Die Summe "+String.valueOf(sum(a, b))+" ist gerade");
+        } else {
+            return("Die Summe "+String.valueOf(sum(a, b))+" ist nicht gerade");
+        } 
+    }
 }
